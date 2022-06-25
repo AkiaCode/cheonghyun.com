0d3e815bb86226efdd891c8f43c401239d6f18ad
AkiaCode <71239005+AkiaCode@users.noreply.github.com>
2022-06-26 02:23:05 +0900

    Add files via upload

--- chlee.js
+++ chlee.js
@@ -3,5 +3,5 @@ div.autoplay = true
 div.muted = true
 div.loop = true
 div.id = 'myVideo'
-document.body.children[0].replaceWith(div)
+document.body.prepend(div)
 div.innerHTML='<source src="' + chrome.runtime.getURL("video.mp4") +'" type="video/mp4">'
--- scripts.js
+++ scripts.js
@@ -1,7 +1,7 @@
 chrome.tabs.onUpdated.addListener((tabID, changeInfo, tab) => {
     if (changeInfo.status == 'complete') {
         if (new URL(tab.url).hostname !== 'www.cheonghyun.com') return
-        chrome.tabs.executeScript(tabID, { file: "chlee.js", runAt: 'document_start' })
-        chrome.tabs.insertCSS(tabID,  { file: 'chlee.css', runAt: 'document_start' })
+        chrome.scripting.executeScript({ files: ["chlee.js"], target: { tabId: tabID }})
+        chrome.scripting.insertCSS({ files: ["chlee.css"], target: { tabId: tabID }})
     }
 })
